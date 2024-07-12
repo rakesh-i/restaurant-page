3ac0ec63746eff0d78a3ef844e5296754794c2c0
Rakesh <rakesh.007ac@gmail.com>
2024-07-12 17:49:07 +0530

    Updates

---
+++ about.bundle.js
@@ -0,0 +1,18 @@
+(self["webpackChunkrestaurant_page"] = self["webpackChunkrestaurant_page"] || []).push([["about"],{
+
+/***/ "./src/about.js":
+/*!**********************!*\
+  !*** ./src/about.js ***!
+  \**********************/
+/***/ (() => {
+
+
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/about.js"));
+/******/ }
+]);
